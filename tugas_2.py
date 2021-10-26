896c4dd1974e5275af0427ce3a0868e64b3a46eb
Kaleb Su <khzputrafsr@gmail.com>
2021-10-26 19:54:02 +0700

    Menambahkan file Latihan1.py

---
+++ tugas_2.py
@@ -0,0 +1,15 @@
+print("-----TUGAS-----")
+print("--BIODATA DIRI--")
+
+nama=input("Nama:")
+call=input("Panggilan:")
+npm=input("NIM:")
+ttl=input("TTL:")
+umur=input("Umur:")
+alamat=input("Alamat:")
+telepon=input("Telepon:")
+
+print("Let me introduce my self. My name is ",nama," but you can call me ",call,"."
+"My npm is ",npm,". I was born in ",ttl," and i am ",umur,"years old. "
+"i am very glad if you want to invite my house in ",alamat,". so dont forget to call me before "
+"the number ",telepon,".")
